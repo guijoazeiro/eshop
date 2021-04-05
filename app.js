50c68280d4e432bcea359663008b81bb5f4f55c1
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2021-04-05 14:47:42 -0300

    Add dotenv

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Hello World')
+const express = require('express')
+const app = express()
+
+require('dotenv/config')
+
+const api = process.env.API_URL
+
+app.get(api+'/', (req, res) =>{
+    res.send('hello')
+})
+
+app.listen(3000, ()=>{
+    console.log(api)
+    console.log('server is running on http://localhost:3000')
+})
