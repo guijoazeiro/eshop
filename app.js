24cc018d9f188d4a6cca9976f2f6792ef670b677
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2021-04-07 13:02:50 -0300

    Add product schema

--- app.js
+++ app.js
@@ -1,41 +1,61 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
-const mongoosee = require('mongoose')
+const mongoose = require('mongoose')
 
 require('dotenv/config')
 
 app.use(express.json())
 app.use(morgan('tiny'))
 
+const productSchema = mongoose.Schema({
+    name: String,
+    image: String,
+    countInStock: {
+        type: Number,
+        required: true
+    }
+})
+
+const Product = mongoose.model('Product', productSchema)
+
 const api = process.env.API_URL
 
-app.get(`${api}/products`, (req, res) =>{
-    const product = {
-        id: 1,
-        name: 'hair dresser',
-        image: 'url.jpg'
+app.get(`${api}/products`, async (req, res) => {
+    const productList = await Product.find()
+    if (!productList) {
+        res.status(500).json({ success: false })
     }
-    res.send(product)
+    res.send(productList)
 })
 
-app.post(`${api}/products`, (req, res) =>{
-    const newProduct = req.body
-    console.log(newProduct)
-    res.send(newProduct)
+app.post(`${api}/products`, (req, res) => {
+    const product = new Product({
+        name: req.body.name,
+        image: req.body.image,
+        countInStock: req.body.countInStock
+    })
+    product.save().then(createdProduct => {
+        res.status(201).json(createdProduct)
+    }).catch((err => {
+        res.status(500).json({
+            error: err,
+            success: false
+        })
+    }))
 })
 
-mongoosee.connect(process.env.URI_MONGODB, {
+mongoose.connect(process.env.URI_MONGODB, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-.then(()=>{
-    console.log('Database Connection is ready....')
-})
-.catch((err)=>{
-    console.error(err)
-})
+    .then(() => {
+        console.log('Database Connection is ready....')
+    })
+    .catch((err) => {
+        console.error(err)
+    })
 
-app.listen(3000, ()=>{    
+app.listen(3000, () => {
     console.log('server is running on http://localhost:3000')
 })
