8ad7437fa48771a09eaa761075894e405a39f3d9
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2021-04-08 15:06:23 -0300

    Add products router

--- app.js
+++ app.js
@@ -5,38 +5,14 @@ const mongoose = require('mongoose')
 
 require('dotenv/config')
 
-const Product = require('./models/product')
+const productsRouter = require('./routers/products')
 
 app.use(express.json())
 app.use(morgan('tiny'))
 
 const api = process.env.API_URL
 
-
-
-app.get(`${api}/products`, async (req, res) => {
-    const productList = await Product.find()
-    if (!productList) {
-        res.status(500).json({ success: false })
-    }
-    res.send(productList)
-})
-
-app.post(`${api}/products`, (req, res) => {
-    const product = new Product({
-        name: req.body.name,
-        image: req.body.image,
-        countInStock: req.body.countInStock
-    })
-    product.save().then(createdProduct => {
-        res.status(201).json(createdProduct)
-    }).catch((err => {
-        res.status(500).json({
-            error: err,
-            success: false
-        })
-    }))
-})
+app.use(`${api}/products`, productsRouter)
 
 mongoose.connect(process.env.URI_MONGODB, {
     useNewUrlParser: true,
