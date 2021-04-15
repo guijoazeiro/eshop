4efe296dc996c87ce0f3044c7ff5aa062b245849
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2021-04-15 13:00:03 -0300

    Add update product

--- routers/products.js
+++ routers/products.js
@@ -4,7 +4,7 @@ const { Category } = require('../models/category')
 const router = express.Router()
 
 router.get(`/`, async (req, res) => {
-    const productList = await Product.find().select('name image -_id')
+    const productList = await Product.find().populate('category')
     if (!productList) {
         res.status(500).json({ success: false })
     }
@@ -12,7 +12,7 @@ router.get(`/`, async (req, res) => {
 })
 
 router.get(`/:id`, async (req, res) => {
-    const product = await Product.findById(req.params.id)
+    const product = await Product.findById(req.params.id).populate('category')
     if (!product) {
         res.status(500).json({ success: false })
     }
@@ -21,8 +21,8 @@ router.get(`/:id`, async (req, res) => {
 
 router.post(`/`, async (req, res) => {
     const category = await Category.findById(req.body.category)
-    if (!category) { 
-        res.status(400).send('Invalid Category') 
+    if (!category) {
+        res.status(400).send('Invalid Category')
     }
 
     let product = new Product({
@@ -43,6 +43,38 @@ router.post(`/`, async (req, res) => {
 
     if (!product) {
         res.status(500).send('The product cannot be created')
+    }
+
+    res.send(product)
+})
+
+router.put('/:id', async (req, res) => {
+
+    const category = await Category.findById(req.body.category)
+    if (!category) {
+        res.status(400).send('Invalid Category')
+    }
+
+    const product = await Product.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            description: req.body.description,
+            richDescription: req.body.description,
+            image: req.body.image,
+            brand: req.body.brand,
+            price: req.body.price,
+            category: req.body.category,
+            countInStock: req.body.countInStock,
+            rating: req.body.rating,
+            numReviews: req.body.numReviews,
+            isFeatured: req.body.isFeatured
+        },
+        { new: true }
+    )
+
+    if(!product){
+        return res.status(500).send('the product cannot be updated')
     }
 
     res.send(product)
