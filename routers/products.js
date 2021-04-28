667182441930ca789b751459f6210c121e194f56
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2021-04-28 12:53:20 -0300

    Add delete product

--- routers/products.js
+++ routers/products.js
@@ -80,4 +80,18 @@ router.put('/:id', async (req, res) => {
     res.send(product)
 })
 
+router.delete('/:id', (req, res) => {
+    Product.findByIdAndRemove(req.params.id)
+        .then(product => {
+            if (product) {
+                return res.status(200).json({ success: true, message: 'the product is deleted!' })
+            }
+            else {
+                return res.status(404).json({ success: false, message: "product not found" })
+            }
+        }).catch(err => {
+            return res.status(400).json({ success: false, error: err })
+        })
+})
+
 module.exports = router
