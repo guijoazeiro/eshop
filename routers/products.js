1b914b45610bea1ac1bafe3876455d3196566e7d
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2021-04-28 13:18:04 -0300

    Add id validation

--- routers/products.js
+++ routers/products.js
@@ -2,6 +2,7 @@ const { Product } = require('../models/product')
 const express = require('express')
 const { Category } = require('../models/category')
 const router = express.Router()
+const mongoose = require('mongoose')
 
 router.get(`/`, async (req, res) => {
     const productList = await Product.find().populate('category')
@@ -49,6 +50,10 @@ router.post(`/`, async (req, res) => {
 })
 
 router.put('/:id', async (req, res) => {
+
+    if(!mongoose.isValidObjectId(req.params.id)){
+        res.status(400).send('Invalid Product id')
+    }
 
     const category = await Category.findById(req.body.category)
     if (!category) {
