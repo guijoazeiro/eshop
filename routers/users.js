0da48bb062c7f88844e593d7b9c2c2ba4f5907de
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2021-04-30 13:17:54 -0300

    Add get user id route

--- routers/users.js
+++ routers/users.js
@@ -9,7 +9,16 @@ router.get(`/`, async (req, res) =>{
     if(!userList) {
         res.status(500).json({success: false})
     } 
-    res.send(userList);
+    res.send(userList)
+})
+
+router.get('/:id', async(req,res)=>{
+    const user = await User.findById(req.params.id).select('-passwordHash');
+
+    if(!user) {
+        res.status(500).json({message: 'The user with the given ID was not found.'})
+    } 
+    res.status(200).send(user)
 })
 
 
