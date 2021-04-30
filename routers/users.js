0f379604232e769297088debb6a7bf11d8eeae38
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2021-04-30 12:49:51 -0300

    Add bcrypt

--- routers/users.js
+++ routers/users.js
@@ -1,13 +1,14 @@
-const { User } = require('../models/user');
-const express = require('express');
-const router = express.Router();
+const { User } = require('../models/user')
+const express = require('express')
+const router = express.Router()
+const bcrypt = require('bcryptjs')
 
 
 router.post('/', async (req, res) => {
     let user = new User({
         name: req.body.name,
         email: req.body.email,
-        passwordHash: req.body.password,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
         phone: req.body.phone,
         isAdmin: req.body.isAdmin,
         street: req.body.street,
@@ -16,12 +17,12 @@ router.post('/', async (req, res) => {
         city: req.body.city,
         country: req.body.country,
     })
-    user = await user.save();
+    user = await user.save()
 
     if (!user)
         return res.status(400).send('the user cannot be created!')
 
-    res.send(user);
+    res.send(user)
 })
 
 module.exports = router
