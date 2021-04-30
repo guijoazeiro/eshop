17fa42f4f3e3edd4bdb28ebced9c75dc50a8b582
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2021-04-30 13:13:00 -0300

    Add get users route

--- routers/users.js
+++ routers/users.js
@@ -3,6 +3,15 @@ const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcryptjs')
 
+router.get(`/`, async (req, res) =>{
+    const userList = await User.find().select('-passwordHash');
+
+    if(!userList) {
+        res.status(500).json({success: false})
+    } 
+    res.send(userList);
+})
+
 
 router.post('/', async (req, res) => {
     let user = new User({
