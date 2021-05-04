adda87fcf6047c1ecea37316eb74d39196441c2b
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2021-05-04 12:08:15 -0300

    Add delete user route

--- routers/users.js
+++ routers/users.js
@@ -69,6 +69,27 @@ router.post('/login', async (req, res) => {
     }
 })
 
+router.post('/register', async (req,res)=>{
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country,
+    })
+    user = await user.save()
+
+    if(!user)
+    return res.status(400).send('the user cannot be created!')
+
+    res.send(user)
+})
+
 router.get('/get/count', async (req, res) => {
     const userCount = await User.countDocuments((count) => count)
 
@@ -78,6 +99,20 @@ router.get('/get/count', async (req, res) => {
     res.send({
         count: userCount
     })
+})
+
+router.delete('/:id', (req, res) => {
+    User.findByIdAndRemove(req.params.id)
+        .then(user => {
+            if (user) {
+                return res.status(200).json({ success: true, message: 'the user is deleted!' })
+            }
+            else {
+                return res.status(404).json({ success: false, message: "user not found" })
+            }
+        }).catch(err => {
+            return res.status(400).json({ success: false, error: err })
+        })
 })
 
 module.exports = router
